a62faa0ab9b25dcf3b41a1e2791e789d964d7556
rsangolli <rakesh.sangolli@yahooinc.com>
2025-06-06 20:10:39 +0530

    Landing page, feedbackOptons

--- app/(main)/dashboard/_component/FeatureAssistant.jsx
+++ app/(main)/dashboard/_component/FeatureAssistant.jsx
@@ -1,8 +1,47 @@
+"use client"
 import React from 'react'
+import { useUser } from '@stackframe/stack'
+import ProfileDialog from './ProfileDialog';
+import { Button } from '@/components/ui/button';
+import { CoachingOptions } from '@/services/Options';
+import Image from 'next/image';
+import { BlurFade } from '@/components/magicui/blur-fade';
 
 function FeatureAssistant() {
+    const user = useUser();
   return (
-    <div>FeatureAssistant</div>
+    <div>
+    <div className='flex justify-between items-center'>
+        <div>
+        <h2 className='font-medium text-gray-500'>My Workspace</h2>
+        <h2 className='text-3xl font-bold'>Welcome back, {user?.displayName}</h2>
+    </div>
+    {/* <ProfileDialog> */}
+                    <Button className='bg-blue-600'>Profile</Button>
+     {/* </ProfileDialog> */}
+    </div>
+
+    <div className='grid grid-cols-2 lg:grid-cols-5 xl:grid-cols-5 gap-10 mt-10'>
+                {CoachingOptions.map((option, index) => (
+                    <BlurFade key={option.icon} delay={0.25 + index * 0.05} inView>
+                        <div key={index} className='p-3 bg-secondary rounded-3xl flex flex-col
+                     justify-center items-center cursor-pointer'>
+                            {/* <UserInputDialog coachingOption={option} > */}
+                                <div key={index} className='flex flex-col
+                     justify-center items-center'>
+                                    <Image src={option.icon} alt={option.name}
+                                        width={150}
+                                        height={150}
+                                        className='h-[70px] w-[70px] hover:rotate-12 cursor-pointer transition-all'
+                                    />
+                                    <h2 className='mt-2'>{option.name}</h2>
+                                </div>
+                            {/* </UserInputDialog> */}
+                        </div>
+                </BlurFade>
+                ))}
+            </div>
+    </div>
   )
 }
 
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import FeatureAssistant from './_component/FeatureAssistant'
+import History from './_component/History'
+import Feedback from './_component/Feedback'
 
 function Dashboard() {
   return (
     <div>
         <FeatureAssistant/>
+
+        <div>
+            <History />
+            {/* <Feedback /> */}
+        </div>
     </div>
   )
 }
